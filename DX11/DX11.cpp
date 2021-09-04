254e54a8be6dcf0b65719abd37671ca1af60dbe3
LostKnowledge33 <rlzamon123@gmail.com>
2021-09-04 16:22:26 +0900

    Commit test

--- DX11/DX11.cpp
+++ DX11/DX11.cpp
@@ -8,6 +8,7 @@
 
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
+HWND hWnd;
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
@@ -97,8 +98,14 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   RECT rc = { 0, 0, 1920, 1080 };
+   AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, false);
+
+   hWnd = CreateWindowW(szWindowClass, szTitle, 
+       WS_OVERLAPPEDWINDOW,
+       WIN_START_X, WIN_START_Y, 
+       rc.right - rc.left, rc.bottom - rc.top, 
+       nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
---
+++ DX11/Defines.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define WIN_START_X 350
+#define WIN_START_Y 150
